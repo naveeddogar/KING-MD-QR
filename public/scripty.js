809b7bcc4600354f5dc8ae4befda012c4fdcfe21
Naveed Dogar <111642055+naveeddogar@users.noreply.github.com>
2023-11-24 17:36:58 +0500

    Delete public directory

--- public/scripty.js
+++
@@ -1,7 +0,0 @@
-var cards = document.getElementsByClassName('card');
-
-Array.prototype.forEach.call(cards, function(el, index, array){  
-  el.addEventListener('click', function() {
-    el.classList.toggle('flipped');
-  });
-});
